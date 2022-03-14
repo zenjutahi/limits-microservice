672ccb100d671b6db1e66ac9ea2b32a40b48d4d3
zenjutahi <zenjutahi@gmail.com>
2022-03-14 13:29:20 +0300

    basic limits service

--- src/main/java/com/learning/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/learning/microservices/limitsservice/LimitsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
 
