222f48b4dae69dbbdad94e1c4417fc46b4a85f43
zenjutahi <zenjutahi@gmail.com>
2022-03-14 13:45:44 +0300

    configure configuration entity that picks limits from app prperties

--- src/main/java/com/learning/microservices/limitsservice/contoller/LimitsController.java
+++ src/main/java/com/learning/microservices/limitsservice/contoller/LimitsController.java
@@ -2,14 +2,20 @@ package com.learning.microservices.limitsservice.contoller;
 
 
 import com.learning.microservices.limitsservice.bean.Limits;
+import com.learning.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,1000);
+        return  new Limits(configuration.getMinimum(), configuration.getMaximum());
+        //return new Limits(1,1000);
     }
 }
